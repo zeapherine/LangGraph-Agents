8d0fb3f5d9630c5eac05768760c461e72220f3a5
Zeapherine Islary <zeapherineislary@Mac.lan>
2025-06-09 20:41:38 +0530

    Add .gitignore and update Agent_Bot.py for Groq API key and Llama3 model

--- Agents/Agent_Bot.py
+++ Agents/Agent_Bot.py
@@ -3,13 +3,18 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END
 from dotenv import load_dotenv # used to store secret stuff like API keys or configuration values
+import os
 
 load_dotenv()
 
 class AgentState(TypedDict):
     messages: List[HumanMessage]
 
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatOpenAI(
+    model="llama3-70b-8192",
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
 
 def process(state: AgentState) -> AgentState:
     response = llm.invoke(state["messages"])
